ba900c15b0c560b237054f05f222c6d57daa103c
cleitus <christiannader@hotmail.com>
2021-06-13 21:01:25 +0200

    readme + small bug

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # bowling
+
+download jar and run the following :
+
+```shell
+java -jar bowling.jar "x x x x x x x x x xxx"
+```
+
+result will be:
+```shell
+Total points: 300
+```
+
+you can also get the points for a given frame between 1 & 10, for example:
+```shell
+java -jar bowling.jar "x x x x x x x x x xxx" 5
+```
+
+result will be :
+```shell
+Total points: 300
+Frame number 5 points: 30
+```
--- src/in/chris/Main.java
+++ src/in/chris/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         BowlingGame game = new BowlingGame(args[0]);
         System.out.println("Total points: " + game.totalPoints());
-        if (args[1] != null) {
+        if (args.length > 1) {
             int frameNum = Integer.parseInt(args[1]);
             System.out.println("Frame number " + frameNum + " points: " + game.getFrame(frameNum).points());
         }
